763df18f3fe0a19f14c9930f2641b2cf0e124cf3
Mads Sülau Valstorp Jørgensen <mads@sulau.dk>
2013-08-21 13:24:33 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
     name="fabric-coat",
-    version="0.4.7",
+    version="0.4.8",
     author="Rasmus Schlünsen, Mads Sülau Jørgensen",
     author_email="rs@konstellation.dk, msj@konstellation.dk",
     description=("Deployment helpers for fabric"),
